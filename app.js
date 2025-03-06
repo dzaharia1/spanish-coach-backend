0e9e6cfdd00c1e85e58be23778d5ffed805fd897
Dan Zaharia <zaharia.danny@gmail.com>
2025-03-06 16:29:05 -0500

    switch to 1.5 pro

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const genAI = new GoogleGenerativeAI(apiKey);
 const systemInstruction = systemInstructions;
 
 
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", systemInstruction: systemInstruction });
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro", systemInstruction: systemInstruction });
 
 const generationConfig = { 
   temperature: 1,
