68ac5e43063a03d02e00ad2595c37b1ea4f76c22
Dan Zaharia <zaharia.danny@gmail.com>
2025-07-30 18:26:23 -0400

    Switch to 2.5 flash

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const genAI = new GoogleGenerativeAI(apiKey);
 const systemInstruction = systemInstructions;
 
 
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", systemInstruction: systemInstruction });
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash", systemInstruction: systemInstruction });
 
 const generationConfig = { 
   temperature: 1,
