01a76c1e21a9422279487614585b676f5d8f0b38
Dan Zaharia <zaharia.danny@gmail.com>
2025-09-16 21:01:21 -0400

    Allow the frontend_domains environment variable to contain many urls

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_DOMAINS,
+  origin: process.env.FRONTEND_DOMAINS ? process.env.FRONTEND_DOMAINS.split(',') : false,
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type'],
   credentials: true
